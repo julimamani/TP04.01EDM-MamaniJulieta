2c494390ca03ba605e779ee0f9263d5c1de0c75a
Juli <Juli@LAPTOP-9IA3U065>
2020-05-22 22:22:21 -0300

    Inicializacion del Proyecto

---
+++ src/main/java/com/example/demo/Tp04MamaniJulietaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04MamaniJulietaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04MamaniJulietaApplication.class, args);
+	}
+
+}
