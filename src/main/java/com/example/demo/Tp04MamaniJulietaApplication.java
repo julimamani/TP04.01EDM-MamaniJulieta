373b06315af092b428ec3c85ad721fd93417d2c8
Julieta Mamani <Julieta Mamani@LAPTOP-9IA3U065>
2020-07-10 01:12:07 -0300

    fixture

--- src/main/java/com/example/demo/Tp04MamaniJulietaApplication.java
+++ src/main/java/com/example/demo/Tp04MamaniJulietaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tp04MamaniJulietaApplication {
+public class Tp04MamaniJulietaApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp04MamaniJulietaApplication.class, args);
